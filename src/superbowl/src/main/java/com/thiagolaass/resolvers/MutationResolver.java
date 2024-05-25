77ae3b5d75c4288732b81afd8e19665351154e88
ThiagoLaass <thiagolaass202@gmail.com>
2024-05-25 19:07:55 -0300

    removing unused imports

--- src/superbowl/src/main/java/com/thiagolaass/resolvers/MutationResolver.java
+++ src/superbowl/src/main/java/com/thiagolaass/resolvers/MutationResolver.java
@@ -1,11 +1,6 @@
 package com.thiagolaass.resolvers;
 
-import org.hibernate.graph.Graph;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.thiagolaass.models.Placar;
-
 import graphql.kickstart.tools.GraphQLQueryResolver;
 
 @Component
--- src/superbowl/src/main/java/com/thiagolaass/resolvers/QueryResolver.java
+++ src/superbowl/src/main/java/com/thiagolaass/resolvers/QueryResolver.java
@@ -1,10 +1,8 @@
 package com.thiagolaass.resolvers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
-
 
 @Component
 public class QueryResolver implements GraphQLQueryResolver{
