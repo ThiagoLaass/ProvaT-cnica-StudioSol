4ccd3c04cd243320ada580d23f4eff880d698676
ThiagoLaass <thiagolaass202@gmail.com>
2024-05-25 19:26:13 -0300

    initiating graphql

--- src/superbowl/src/main/java/com/thiagolaass/models/Placar.java
+++ src/superbowl/src/main/java/com/thiagolaass/models/Placar.java
@@ -1,22 +1,24 @@
 package com.thiagolaass.models;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.ArrayList;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 public class Placar {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private ArrayList<Integer> score;
+    private Integer pointsTime1;
+
+    private Integer pointsTime2;
+
+    private Integer possibilities;
 }
